2dcc1357be4b57928705790051c0f0fadd4ce42a
Estefany Diaz <estefanydiaz75@gmail.com>
2022-03-18 22:06:35 -0500

    route get character

--- src/routes/character.js
+++ src/routes/character.js
@@ -2,6 +2,21 @@ const { Router } = require("express");
 const { Character, Movie, Genre } = require("../db.js");
 const router = Router();
 
+router.get("/", async (req, res) => {
+  try {
+    let characters = await Character.findAll();
+    let character = [];
+    characters?.map((c) =>
+      character.push({
+        image: c.image,
+        name: c.name,
+      })
+    );
+    res.status(200).send(character);
+  } catch (err) {
+    res.status(404).send("este es el error", err);
+  }
+});
 router.post("/", async (req, res) => {
   const { image, name, age, width, history, movies } = req.body;
   try {
@@ -50,4 +65,5 @@ router.post("/", async (req, res) => {
     res.status(404).send(`No se pudo guardar la info, ${err}`);
   }
 });
+
 module.exports = router;
