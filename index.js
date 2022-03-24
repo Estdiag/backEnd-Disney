f574a4ba3b9d93369b00d63692b1115f53480300
Estefany Diaz <estefanydiaz75@gmail.com>
2022-03-24 17:20:43 -0500

    delete force

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 
-conn.sync({ force: false }).then(() => {
+conn.sync().then(() => {
   server.listen(process.env.PORT || 5000, () => {
     console.log("%s listening at 3001");
   });
